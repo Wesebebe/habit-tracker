853af72dac511226fde4acf6423923c60c270fe5
Reagan <rokhanga@gmail.com>
2024-12-30 12:05:04 +0300

    Initial commit

---
+++ frontend/src/components/HabitList/HabitList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HabitList.css";
+
+const HabitList = () => {
+  return <div></div>;
+};
+
+export default HabitList;
---
+++ frontend/src/components/ProgressTracker/ProgressTracker.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProgressTracker.css";
+
+const ProgressTracker = () => {
+  return <div></div>;
+};
+
+export default ProgressTracker;
