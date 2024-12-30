e26fb8f675081d2a22f88f63e9ea583974d13d0c
Reagan <rokhanga@gmail.com>
2024-12-30 12:43:52 +0300

    Add habit list component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import HabitList from "./components/HabitList/HabitList";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <HabitList />
     </div>
   );
 };
--- frontend/src/components/HabitList/HabitList.jsx
+++ frontend/src/components/HabitList/HabitList.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HabitList.css";
 
 const HabitList = () => {
-  return <div></div>;
+  const [habits, setHabits] = useState([
+    { id: 1, name: "Excercise", completed: false },
+    { id: 2, name: "Read", completed: true },
+  ]);
+
+  return (
+    <div className="habit-list">
+      <h2>Your Habits</h2>
+      <ul className="list">
+        {habits.map((habit) => (
+          <li key={habit.id} className="habit">
+            <span>{habit.name}</span>
+            <button>{habit.completed ? "Undo" : "Complete"}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default HabitList;
